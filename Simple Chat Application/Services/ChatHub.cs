71b1d1f4e257292838e8174bae7899faddb7a6d6
alcon <alcon@DESKTOP-Q7OITQN>
2024-07-11 16:01:12 +0800

    Add Comments

--- Simple Chat Application/Services/ChatHub.cs
+++ Simple Chat Application/Services/ChatHub.cs
@@ -16,11 +16,12 @@ namespace Simple_Chat_Application.Services
             _logger = logger;
         }
 
-        //Receive message from the 
+        //Connects a user to the chat and notifies all connected clients.
         public async Task ConnectUser(string userName)
         {
             try
             {
+                // Notify all clients that a user has joined the chat
                 await Clients.All.SendAsync("JoinMessage", $"{userName}", "connectUser");
             }
             catch (Exception ex)
@@ -34,9 +35,6 @@ namespace Simple_Chat_Application.Services
         {
             try
             {
-                // Send the message to all connected clients with the current timestamp
-                await Clients.All.SendAsync("ReceiveMessage", userName, message, DateTime.Now);
-
                 // Create a new chat message entity
                 var chatMessage = new ChatMessage
                 {
@@ -45,9 +43,12 @@ namespace Simple_Chat_Application.Services
                     Timestamp = DateTime.UtcNow
                 };
 
-                // Add the chat message to the database context and save changes asynchronously
+                // Add the chat message to the database context and save changes
                 _dbContext.ChatMessages.Add(chatMessage);
                 await _dbContext.SaveChangesAsync();
+
+                // Send the message to all connected clients with the current timestamp
+                await Clients.All.SendAsync("ReceiveMessage", userName, message, DateTime.Now);
             }
             catch (Exception ex)
             {
@@ -76,7 +77,7 @@ namespace Simple_Chat_Application.Services
             }
         }
 
-        // Helper method to send system messages
+        // Helper method to send system error messages
         private async Task SendSystemErrorMessage(string message)
         {
             await Clients.Caller.SendAsync("ErrorMessage",message);
